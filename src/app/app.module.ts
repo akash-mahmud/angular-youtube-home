eca81d0a5ca38fe91cbda32e7253103bb63c7172
Akash <mahmudakash177@gmail.com>
2023-10-11 14:30:25 +0600

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LucideAngularModule, File, Home, Menu, UserCheck , User, Upload, Bell, Mic , Search} from 'lucide-angular';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LucideAngularModule.pick({File, Home, Menu, UserCheck, User, Upload , Bell, Mic, Search})
+
   ],
   providers: [],
   bootstrap: [AppComponent]
